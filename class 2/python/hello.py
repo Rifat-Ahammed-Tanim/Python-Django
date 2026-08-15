f95465c98c6e67789608d19b5231c0e97f227248
Rifat-Ahammed-Tanim <tanimofficial18@gmail.com>
2026-08-15 22:10:51 +0600

    first commit

---
+++ class 2/python/hello.py
@@ -0,0 +1,25 @@
+print("what is your name?");
+
+# name = input();
+
+# print("Nice to meet you, " + name);
+print("How old are you?");
+
+# age = input();
+
+# print("You are " + age + " years old.");
+print(5+5)
+
+"""
+the name of out country is bangladesh
+it's becoming a developed country in 2041  
+"""
+
+a = 100
+print(type(a))
+
+b = "200"
+print(type(b))
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(z)
